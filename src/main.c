117f1cf1199179544b6cb6309c44fe4fb8f03a78
ccryssia <gshine@m.scnu.edu.cn>
2024-12-04 19:24:09 +0800

    Support jump and halt instructions

--- src/main.c
+++ src/main.c
@@ -2,9 +2,11 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define ARRAY_SIZE(xs) (sizeof(xs) / sizeof(xs[0]))
 #define LIM_STACK_CAPACITY 1024
+#define LIM_PROGRAM_CAPACITY 1024
 
 typedef enum {
     TRAP_OK = 0,
@@ -12,6 +14,7 @@ typedef enum {
     TRAP_STACK_UNDERFLOW,
     TRAP_DIV_BY_ZERO,
     TRAP_ILLEGAL_INST,
+    TRAP_ILLEGAL_INST_ACCESS,
 } Trap;
 
 const char *trap_as_cstr(Trap trap)
@@ -27,6 +30,8 @@ const char *trap_as_cstr(Trap trap)
         return "TRAP_DIV_BY_ZERO";
     case TRAP_ILLEGAL_INST:
         return "TRAP_ILLEGAL_INST";
+    case TRAP_ILLEGAL_INST_ACCESS:
+        return "TRAP_ILLEGAL_INST_ACCESS";
     default:
         assert(0 && "trap_as_cstr: unreachable");
     }
@@ -34,17 +39,14 @@ const char *trap_as_cstr(Trap trap)
 
 typedef int64_t Word;
 
-typedef struct {
-    Word stack[LIM_STACK_CAPACITY];
-    size_t stack_size;
-} Lim;
-
 typedef enum {
     INST_PUSH,
     INST_PLUS,
     INST_MINUS,
     INST_MULT,
     INST_DIV,
+    INST_JMP,
+    INST_HALT,
 } Inst_Type;
 
 const char *inst_type_as_cstr(Inst_Type type)
@@ -60,6 +62,10 @@ const char *inst_type_as_cstr(Inst_Type type)
         return "INST_MULT";
     case INST_DIV:
         return "INST_DIV";
+    case INST_JMP:
+        return "INST_JMP";
+    case INST_HALT:
+        return "INST_HALT";
     default:
         assert(0 && "inst_type_as_cstr: unreachable");
     }
@@ -95,14 +101,44 @@ typedef struct {
         .type = INST_DIV                 \
     }
 
-Trap lim_execute_inst(Lim *lim, Inst inst)
+#define /*Inst*/ MAKE_INST_JMP(/*Word*/ addr) \
+    {                                         \
+        .type = INST_JMP, .operand = (addr),  \
+    }
+
+#define /*Inst*/ MAKE_INST_HALT(/*void*/) \
+    {                                     \
+        .type = INST_HALT                 \
+    }
+
+/* Lisp Virtual Machine */
+typedef struct {
+    /* Stack */
+    Word stack[LIM_STACK_CAPACITY];
+    Word stack_size;
+    /* Code */
+    Inst program[LIM_PROGRAM_CAPACITY];
+    Word program_size;
+    /* State */
+    Word ip;
+    int halt;
+} Lim;
+
+Trap lim_execute_inst(Lim *lim)
 {
+    if (lim->ip < 0 || lim->ip >= lim->program_size) {
+        return TRAP_ILLEGAL_INST_ACCESS;
+    }
+
+    Inst inst = lim->program[lim->ip];
+    /* DEBUG */ printf("%s\n", inst_type_as_cstr(inst.type));
     switch (inst.type) {
     case INST_PUSH:
         if (lim->stack_size >= LIM_STACK_CAPACITY) {
             return TRAP_STACK_OVERFLOW;
         }
         lim->stack[lim->stack_size++] = inst.operand;
+        lim->ip++;
         break;
 
     case INST_PLUS:
@@ -111,6 +147,7 @@ Trap lim_execute_inst(Lim *lim, Inst inst)
         }
         lim->stack[lim->stack_size - 2] += lim->stack[lim->stack_size - 1];
         lim->stack_size--;
+        lim->ip++;
         break;
 
     case INST_MINUS:
@@ -119,6 +156,7 @@ Trap lim_execute_inst(Lim *lim, Inst inst)
         }
         lim->stack[lim->stack_size - 2] -= lim->stack[lim->stack_size - 1];
         lim->stack_size--;
+        lim->ip++;
         break;
 
     case INST_MULT:
@@ -127,6 +165,7 @@ Trap lim_execute_inst(Lim *lim, Inst inst)
         }
         lim->stack[lim->stack_size - 2] *= lim->stack[lim->stack_size - 1];
         lim->stack_size--;
+        lim->ip++;
         break;
 
     case INST_DIV:
@@ -138,6 +177,18 @@ Trap lim_execute_inst(Lim *lim, Inst inst)
         }
         lim->stack[lim->stack_size - 2] /= lim->stack[lim->stack_size - 1];
         lim->stack_size--;
+        lim->ip++;
+        break;
+
+    case INST_JMP:
+        // In this instruction, its address counld be illegal, since during next
+        // instruction execution this function would return trap/error
+        // TRAP_ILLEGAL_INST_ACCESS.
+        lim->ip = inst.operand;
+        break;
+
+    case INST_HALT:
+        lim->halt = 1;
         break;
 
     default:
@@ -146,11 +197,19 @@ Trap lim_execute_inst(Lim *lim, Inst inst)
     return TRAP_OK;
 }
 
-void lim_dump(FILE *stream, Lim *lim)
+void lim_load_program_from_memory(Lim *lim, Inst *program, Word program_size)
+{
+    assert(program_size > 0 && program_size <= LIM_PROGRAM_CAPACITY);
+
+    memcpy(lim->program, program, sizeof(program[0]) * program_size);
+    lim->program_size = program_size;
+}
+
+void lim_dump_stack(FILE *stream, Lim *lim)
 {
     fprintf(stream, "Stack:\n");
     if (lim->stack_size > 0) {
-        for (size_t i = 0; i < lim->stack_size; i++) {
+        for (Word i = 0; i < lim->stack_size; i++) {
             fprintf(stream, "  %ld\n", lim->stack[i]);
         }
     } else {
@@ -163,22 +222,23 @@ Lim lim = {0};
 Inst program[] = {
     MAKE_INST_PUSH(69), MAKE_INST_PUSH(22), MAKE_INST_PUSH(43),
     MAKE_INST_PLUS(),   MAKE_INST_MINUS(),  MAKE_INST_PUSH(21),
-    MAKE_INST_PUSH(3),  MAKE_INST_DIV(),    MAKE_INST_MULT(),
+    MAKE_INST_JMP(9),   MAKE_INST_PUSH(3),  MAKE_INST_DIV(),
+    MAKE_INST_MULT(),   MAKE_INST_HALT(),
 };
 
 int main()
 {
-    lim_dump(stdout, &lim);
-    for (size_t i = 0; i < ARRAY_SIZE(program); i++) {
-        Trap trap = lim_execute_inst(&lim, program[i]);
-        /* DEBUG */ printf("%s\n", inst_type_as_cstr(program[i].type));
-        /* DEBUG */ lim_dump(stdout, &lim);
+    lim_load_program_from_memory(&lim, program, ARRAY_SIZE(program));
+    lim_dump_stack(stdout, &lim);
+    while (!lim.halt) {
+        Trap trap = lim_execute_inst(&lim);
+        /* DEBUG */ lim_dump_stack(stdout, &lim);
         if (trap != TRAP_OK) {
             fprintf(stderr, "Trap activated: %s\n", trap_as_cstr(trap));
-            lim_dump(stderr, &lim);
+            lim_dump_stack(stderr, &lim);
             exit(1);
         }
     }
-    lim_dump(stdout, &lim);
+    lim_dump_stack(stdout, &lim);
     return 0;
 }
